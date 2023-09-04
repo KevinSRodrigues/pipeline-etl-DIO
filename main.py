fe152dbb2c6d3f1b11db138723ae1f04e598140b
Kévin da Silva Rodrigues <kelvinrodri@hotmail.com>
2023-09-04 10:56:30 -0300

    Upgrade: Func. get_code

--- main.py
+++ main.py
@@ -2,11 +2,8 @@ import pandas as pd
 import requests
 import json
 
-# Pandas
 
 sites = pd.read_csv('Lista_sites.csv')['Sites'].tolist()
-
-# URLScan
 
 
 def urlscan(site):
@@ -15,10 +12,28 @@ def urlscan(site):
     data = {"url": f"{site}", "visibility": "private"}
     response = requests.post(
         'https://urlscan.io/api/v1/scan/', headers=headers, data=json.dumps(data))
+
     return response, response.json()
+
+
+def get_code(response):
+    code = list(response)[0]
+    response_code = str(code)
+    inicio = response_code.find("[")
+    fim = response_code.rfind("]")
+    codigo_status = response_code[inicio + 1:fim]
+    codigo_status = int(codigo_status)
+
+    return codigo_status
 
 
 if __name__ == '__main__':
     for site in sites:
         verificacao = urlscan(site)
-        print(f'\n{site}:', f'\n{verificacao}\n')
+        code_scan = get_code(verificacao)
+        if code_scan == 200:
+            print(f'\n{site}: Sucesso!\nCódigo HTTP 200\n')
+        elif code_scan == 400:
+            print(f'\n{site}: Erro!\nCódigo HTTP 400\n')
+        else:
+            print('Código não especificado')
